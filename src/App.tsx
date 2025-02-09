7e29d2bc2a18add0d75949df618f61e98b336616
efar716 <110000165+efar716@users.noreply.github.com>
2025-02-09 18:44:58 +1300

    scan page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ScannerPage from './pages/ScannerPage'
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ScannerPage />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
