8f076a515c93a76d0b068d136daa7968bff048b1
efar716 <110000165+efar716@users.noreply.github.com>
2025-02-26 00:40:12 +1300

    restyled recipe page

--- src/pages/RecipeInformation.tsx
+++ src/pages/RecipeInformation.tsx
@@ -6,39 +6,47 @@ import DOMPurify from 'dompurify';
 
 
 const RecipeInformation = () => {
+    const recipeTestDummy: Recipe = {
+        id: 1,
+        name: "Spaghetti Meatball Pasta",
+        image: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+        summary: "Spaghetti with meatballs is an Italian-American dish that usually consists of spaghetti, tomato sauce and meatballs Italian-American cuisine is a style of Italian cuisine adapted throughout the United States of America. Italian-American food is based on the food of Southern Italy, heavily influenced by Italian",
+        diet: ["Vegetarian", "Vegan"],
+        ingredients: ["Spaghetti", "Tomato Sauce", "Meatballs"]
+    }
     const { recipeId } = useParams();
-    const [recipe, setRecipe] = useState<Recipe | null>(null);
-    useEffect(() => {
-
-        fetch(`http://127.0.0.1:5001/scan2eat-8058d/us-central1/getRecipe?recipeId=${recipeId}`).then(
-            response => response.json()).then(
-                data => { setRecipe(data); console.dir(data) }
-            );
-
-    }, [recipeId]);
+    /*  const [recipe, setRecipe] = useState<Recipe | null>(null);
+     useEffect(() => {
+ 
+         fetch(`http://127.0.0.1:5001/scan2eat-8058d/us-central1/getRecipe?recipeId=${recipeId}`).then(
+             response => response.json()).then(
+                 data => { setRecipe(data); console.dir(data) }
+             );
+ 
+     }, [recipeId]); */
     return (
 
-        <main>
-            {recipe ? ( // Conditionally render based on whether recipe exists
-                <>
-                    <section className='header-background header-background-seperator'>
-                        <h1>{recipe.name}</h1>
-                    </section>
-
+        <>
+            {recipeTestDummy ? ( // Conditionally render based on whether recipe exists
+                <main>
+                    {/*  <section className='header-background header-background-seperator'>
+                        <h1>{recipeTestDummy.name}</h1>
+                    </section> */}
+                    <img src={recipeTestDummy.image} alt={recipeTestDummy.name} />
                     <section className='page-content'>
-                        <img src={recipe.image} alt={recipe.name} />
-                        <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(recipe.summary || '') }}></p>
+
+                        <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(recipeTestDummy.summary || '') }}></p>
 
 
 
-                        <InfoSection title="DIET" content={recipe.diet?.toString() || "Could not load data"} />
-                        <InfoSection title="INGREDIENTS" content={recipe.ingredients?.toString() || "Could not load data"} />
+                        <InfoSection title="DIET" content={recipeTestDummy.diet?.toString() || "Could not load data"} />
+                        <InfoSection title="INGREDIENTS" content={recipeTestDummy.ingredients?.toString() || "Could not load data"} />
                     </section>
-                </>
+                </main>
             ) : (
                 <p>Loading...</p> // Show loading message if recipe is not yet available
             )}
-        </main>
+        </>
     )
 }
 
