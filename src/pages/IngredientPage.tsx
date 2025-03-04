1dc025326101f1fc9469ee4139030cd0c50a8b65
efar716 <110000165+efar716@users.noreply.github.com>
2025-03-04 17:03:10 +1300

    Ingredient page text generation

--- src/pages/IngredientPage.tsx
+++ src/pages/IngredientPage.tsx
@@ -3,13 +3,43 @@ import InfoSection from '../components/InfoSection';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+// Generate description based on ingredient data
+const generateDescription = (description: {
+    fat: string,
+    salt: string,
+    sugars: string,
+    saturatedFat: string,
+}) => {
+
+    const descriptionString = `This product is ${description.fat} in fat, ${description.saturatedFat} in saturated fat, ${description.sugars} in sugars, ${description.salt} in salt`;
+    return descriptionString;
+}
+
 const IngredientPage = () => {
 
     const [ingredient, setIngredient] = useState<Ingredient | null>(null);
     const { ingredientId } = useParams();
+
+    const [neutrionalValue, setNutritionalValue] = useState<string>("");
+    const [productDescription, setProductDescription] = useState<string>("");
+
     useEffect(() => {
-        console.log(ingredientId)
+        console.log(ingredientId);
+        fetch(`http://127.0.0.1:5001/scan2eat-8058d/us-central1/getIngredient?barCode=${ingredientId}`).then(
+            response => response.json()).then(
+                data => { setIngredient(data); console.dir(data) }
+            );
     }, [ingredientId]);
+
+    useEffect(() => {
+        if (ingredient?.nutritionalGrade) {
+            setNutritionalValue(ingredient.nutritionalGrade);
+        }
+
+        const description = generateDescription(ingredient?.description || { fat: "0g", salt: "0g", sugars: "0g", saturatedFat: "0g" });
+        setProductDescription(description);
+
+    }, [ingredient]);
     return (
         (ingredient ? (<main  >
             <section className='header-background header-background-seperator'>
@@ -18,11 +48,11 @@ const IngredientPage = () => {
 
             <section className='page-content'>
 
-                <InfoSection title="Nutritional Value" content="HIGH" />
+                <InfoSection title="Nutritional Value" content={neutrionalValue} />
 
                 <InfoSection title="Allergens" content="NUTS, SOYBEANS" />
 
-                <p>This product is high in sugar, fat, and saturated fat but low in salt</p>
+                <p>{productDescription}</p>
             </section>
 
             <button>View Recipes</button>
